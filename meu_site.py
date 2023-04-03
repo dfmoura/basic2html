ae61edcc95b193f6c96e752ec8626d01ffe09df5
Diogo Moura <diogomou@gmail.com>
2023-04-02 22:30:27 -0300

    Novo site railway

---
+++ meu_site.py
@@ -0,0 +1,15 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+# route -> teste.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
